258f8596c116fd7dd2ada36a8d5cff6239c56743
JustinAronson <justinaronson@gmail.com>
2023-04-11 19:34:57 -0500

    Adding openAI

--- parse.py
+++ parse.py
@@ -1,8 +1,14 @@
 import nltk
+from opinionOrFactSentenceClassifier.quick_tests.py import * 
+
+#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
+print('In parse.py')
 
 def splitArticle():
     tokenizer = nltk.data.load('./english.pickle')
     data = open('Article.txt').read()
+    return nltk.tokenize.sent_tokenize(data, language='english')
+
     splitArticle = tokenizer.tokenize(data)
 
     return splitArticle
@@ -11,4 +17,12 @@ print('TEST')
 
 articleArray = splitArticle()
 for sentence in articleArray:
-    print('Sentence: ' + sentence)
+    test_sent = 'This donut is quite possibly the best tasting donut in the entire world.'
+    curr = sample(test_sent, nn_classifier_scaled, scaler)
+    count_f = 0
+    count_d = 0
+    if curr:
+        count_f += 1
+    else:
+        count_d += 1
+    
