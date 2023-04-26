79888c6eb04497340f115feb034e9128e50c0e06
JustinAronson <justinaronson@gmail.com>
2023-04-25 20:03:00 -0500

    Trouble importing react

--- content.js
+++ content.js
@@ -1,14 +1,11 @@
 import('./Articles.js')
-import('react')
-// import React from 'react';
+import('./reactApp/react-chrome-app/node_modules/react')
+
 // content.js
 alert('Hello, world!');
 createList(["asdfasdf","asdfasdfdfdfd"])
 
 // Call categorizer from python
 
-function createList(data) {
-
-    return <Articles props={data}></Articles>
-    
+function createList(data) {    return <Articles props={data}></Articles>
 }
