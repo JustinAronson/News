e1d86b40c32ef98971e41c62f6b6aeccb4cc602b
JustinAronson <justinaronson@gmail.com>
2023-05-01 13:28:55 -0500

    Tried to import babel, couldn't run extension

--- reactApp/react-chrome-app/src/content.js
+++ reactApp/react-chrome-app/src/content.js
@@ -1,14 +1,11 @@
-import('./Articles.js')
-import('react')
-// import React from 'react';
-// content.js
-alert('Hello, world!');
-createList(["asdfasdf","asdfasdfdfdfd"])
-
+// import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Articles from './Articles';
 // Call categorizer from python
-
 function createList(data) {
-
-    return <Articles props={data}></Articles>
-    
+  console.log("In src Folder");
+  // render the Articles component to the DOM
+  ReactDOM.render(<Articles props={data} />, document.getElementById('root'));
 }
+createList(["asdfasdf","asdfasdfdfdfd"]);
---
+++ reactApp/react-chrome-app/src/content_1.21.js
@@ -0,0 +1,15 @@
+import('./Articles.js')
+import('react')
+// import React from 'react';
+// content.js
+alert('Hello, world!');
+createList(["asdfasdf","asdfasdfdfdfd"])
+
+// Call categorizer from python
+
+function createList(data) {
+
+    console.log("In src Folder")
+    return <Articles props={data}></Articles>
+    
+}
