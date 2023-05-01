35a151808ba1d090b83844fd9580243b7e1e3fec
JustinAronson <justinaronson@gmail.com>
2023-05-01 15:46:40 -0500

    Added fetch function

--- panda/src/App.js
+++ panda/src/App.js
@@ -2,17 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Articles from './Articles.js';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
         <input type="text" placeholder="Search" name="searchBox"></input>
-        
-        <Articles/>
+        <button id="fetch-button">Fetch JSON Data</button>
+        <Articles />
       </header>
     </div>
   );
 }
+
+async function logJSONData() {
+  const response = await fetch("http://example.com/movies.json");
+  const jsonData = await response.json();
+  console.log(jsonData);
+}
+
+const fetchButton = document.getElementById("fetch-button");
+fetchButton.addEventListener("click", logJSONData);
 
 export default App;
