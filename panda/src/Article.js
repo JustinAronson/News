acaaa03418f3c3e4f9011d4e68dee5def288ce12
rnewkirk <ryannewkirk2024@u.northwestern.edu>
2023-05-09 17:29:22 -0500

    articleTest in new dict format shows up now yay

--- panda/src/Article.js
+++ panda/src/Article.js
@@ -1,15 +1,16 @@
 const Article = (props) => {
 
     console.log("In article")
-    console.log(props["props"][0])
-    console.log('Printing props[1]')
+    console.log(props["props"])
     console.log(props["props"][1])
     console.log('Done printing')
 
     return (
       <div class="article">
-        <p>{props["props"]}</p>
-        {props.key}
+        <h3>{props["props"][0]}</h3>
+        <p>{props["props"][1]["url"]} </p>
+        <p>{props["props"][1]["dataDrivenIndex"]}  </p>
+        <p>{props["props"][1]["anecdotalIndex"]}  </p>
       </div>
     )
     
