3d8d7ca4522c78dcfbfa83204912e733c17afbf8
rnewkirk <ryannewkirk2024@u.northwestern.edu>
2023-05-15 06:03:59 -0500

    update, haven't checked if works w/ python though

--- panda/src/App.js
+++ panda/src/App.js
@@ -54,20 +54,55 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <input 
-            id="searchInput"
-            name="searchInput"
-            type="text" 
-            placeholder="Search"
-            onChange={event => setSearchInput(event.target.value)}
-            value={searchInput}></input>
-          <button type="submit">Submit form</button>
-        </form>
-        
 
-      </header>
+      <div className="shadow bg-gray-800">
+
+            <ul class="flex py-2 shadow bg-gray-800">
+              <li class="flex-1 mr-2">
+                  <p className="font-bold text-2xl text-gray-300">PANDA</p>
+              </li>
+              <li class="flex-1 mr-2"></li>
+              <li class="flex-1 mr-2"></li>
+              <li class="flex-1 mr-2">
+              <a href="#" class=" transition-colors duration-300 transform text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">Home</a>
+            </li>
+            <li class="flex-1 mr-2">
+              <a href="#" class="border-b-2 border-transparent transition-colors duration-300 transform text-gray-300 hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">About</a>
+            </li>
+            <li class="text-center flex-1">
+            <a href="https://github.com/JustinAronson/News/tree/reactWebsite" class="border-b-2 border-transparent transition-colors duration-300 transform text-gray-300 hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">GitHub</a>
+            </li>
+            </ul>
+        </div>
+
+
+      <div class="max-w-2xl p-8">
+          <label class="block text-left text-lg font-medium text-gray-800 pb-4">Search Articles</label>
+          
+          <form>   
+              <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+              <div class="relative">
+                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                  </div>
+                  <input 
+                    onChange={event => setSearchInput(event.target.value)}
+                    value={searchInput}
+                    placeholder="Search News Articles..." 
+                    type="search" 
+                    name="searchInput"
+                    id="default-search" 
+                    class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    required/>
+                    <button 
+                      type="submit" 
+                      class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Search
+                    </button>
+              </div>
+          </form>
+      </div>
+
       <div>
         <Articles props={articleTest}/>
         {/*articleDict ? <Articles props={articleDict}/> : <div></div>*/}
